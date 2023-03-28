8c10f424bbf896030e676989ce26b1be2082bafd
Abdulhaseebimran <haseebimran0001@gmail.com>
2023-03-28 19:45:38 +0500

    solve pattern question

---
+++ conditional statements/pattern practice/pattern13.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int row = 1;
+    char start = 'A';
+    while (row <= n)
+    {
+        int col = 1;
+        while (col <= n)
+        {
+            cout << start;
+            start = start + 1;
+            col = col + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    }
+}
+
+// task complete
